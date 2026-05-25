d47bbf9f65ad3710eb01126813c2a2fef3bfccae
phucphucc <tvphuc.11thd@gmail.com>
2026-05-25 13:35:15 +0700

    init project

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,17 +1,68 @@
 import { defineConfig, globalIgnores } from "eslint/config";
 import nextVitals from "eslint-config-next/core-web-vitals";
 import nextTs from "eslint-config-next/typescript";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import react from "eslint-plugin-react";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import unusedImports from "eslint-plugin-unused-imports";
 
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+  prettierRecommended,
+  {
+    plugins: {
+      "simple-import-sort": simpleImportSort,
+      "unused-imports": unusedImports,
+      react: react,
+    },
+    rules: {
+      "simple-import-sort/imports": "error",
+      "simple-import-sort/exports": "error",
+
+      "unused-imports/no-unused-imports": "error",
+      "unused-imports/no-unused-vars": [
+        "warn",
+        {
+          vars: "all",
+          varsIgnorePattern: "^_",
+          args: "after-used",
+          argsIgnorePattern: "^_",
+        },
+      ],
+
+      "no-unused-vars": "off",
+
+      "@next/next/no-img-element": "off",
+
+      "react/jsx-no-literals": [
+        "error",
+        {
+          noStrings: true,
+          allowedStrings: [
+            ":",
+            " ",
+            ": ",
+            "-",
+            " - ",
+            "—",
+            "•",
+            "·",
+            "đ",
+            ".",
+            "/",
+          ],
+          ignoreProps: true,
+        },
+      ],
+    },
+  },
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
     "next-env.d.ts",
+    "node_modules/**",
   ]),
 ]);
 
